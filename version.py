c65d250b3491a5edf76abfd298155f21f5071f97
Deel <deel@d-l.top>
2016-01-02 22:17:30 +0800

    fix

--- version.py
+++ version.py
@@ -4,3 +4,4 @@ script_name   = 'socks-proxy-tester'
 __version__   = '0.1.0'
 upgrade_check = 'https://raw.githubusercontent.com/DD-L/socks-proxy-tester/master/version.py'
 filelist      = ['version.py', 'socks-proxy-test.py']
+path          = 'https://raw.githubusercontent.com/DD-L/socks-proxy-tester/master/'
