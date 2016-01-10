1125e80f883b513140bd15df1a5e1018e70ffe17
Deel <deel@d-l.top>
2016-01-10 13:43:53 +0800

    fixed version info

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 script_name   = 'socks-proxy-tester'
-__version__   = '0.1.0'
+__version__   = '0.1.1'
 upgrade_check = 'https://raw.githubusercontent.com/DD-L/socks-proxy-tester/master/version.py'
 filelist      = ['version.py', 'socks-proxy-test.py']
 path          = 'https://raw.githubusercontent.com/DD-L/socks-proxy-tester/master/'
